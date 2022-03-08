d6242d6e62ce483fb6cf2afd6306ae9c51fa0fe6
Cleyson L <clbbdev@gmail.com>
2022-03-08 03:01:22 -0300

    add minor improvements

--- src/stories/Error.stories.tsx
+++ src/stories/Error.stories.tsx
@@ -1,16 +1,16 @@
-import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 
 import ErrorComponent from '../components/Error';
 
 export default {
-    title: 'Error',
+    title: 'Components/Error Component',
     component: ErrorComponent,
 } as ComponentMeta<typeof ErrorComponent>;
 
 const Template: ComponentStory<typeof ErrorComponent> = (args) => <ErrorComponent {...args} />;
 
 export const ErrorMessage = Template.bind({});
+ErrorMessage.storyName = 'Error Component';
 ErrorMessage.args = {
     message: 'Error message',
 }
